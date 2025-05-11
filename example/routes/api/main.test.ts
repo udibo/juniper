fbb68fee6312201991684a51e7b319cdbfa5b5c8
Kyle June <github@kylejune.com>
2025-05-10 23:27:15 -0400

    Add coverage for empty route folder

--- example/routes/api/main.test.ts
+++ example/routes/api/main.test.ts
@@ -4,8 +4,20 @@ import { assertEquals } from "@std/assert";
 import { app } from "/main.ts";
 
 describe("GET /api", () => {
-  it("should return hello world", async () => {
+  it("should return not found error", async () => {
     const res = await app.request("http://localhost/api");
+    assertEquals(res.status, 404);
+    assertEquals(await res.json(), {
+      status: 404,
+      title: "NotFoundError",
+      detail: "Not found",
+    });
+  });
+});
+
+describe("GET /api/empty", () => {
+  it("should return not found error", async () => {
+    const res = await app.request("http://localhost/empty");
     assertEquals(res.status, 404);
     assertEquals(await res.json(), {
       status: 404,
