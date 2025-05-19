4bc5cabf72dca20e6ae3d8b5c64c436127679219
Kyle June <github@kylejune.com>
2025-05-18 22:20:19 -0400

    Add active spans to service class methods

--- example/services/service.ts
+++ example/services/service.ts
@@ -3,6 +3,8 @@ import { generate as generateUUIDv7 } from "@std/uuid/unstable-v7";
 import { isTest } from "@udibo/juniper/utils/env";
 import { HttpError } from "@udibo/http-error";
 
+import { startActiveSpan } from "/utils/otel.ts";
+
 export interface Entity {
   id: string;
   createdAt: Date;
@@ -52,276 +54,323 @@ export class Service<T extends Entity> implements Disposable {
   }
 
   parse(data: unknown): T {
-    try {
-      return this.schema.parse(data) as T;
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        const messages = error.errors.map((e) => {
-          if (e.message === "Required" && e.path && e.path.length > 0) {
-            return `Field '${e.path.join(".")}' is required.`;
-          }
-          return e.message;
-        });
+    return startActiveSpan("parse", (span) => {
+      span.setAttribute("service", this.name);
+      try {
+        return this.schema.parse(data) as T;
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          const messages = error.errors.map((e) => {
+            if (e.message === "Required" && e.path && e.path.length > 0) {
+              return `Field '${e.path.join(".")}' is required.`;
+            }
+            return e.message;
+          });
+          throw new HttpError(
+            400,
+            `Invalid ${this.name} data: ${messages.join(", ")}`,
+          );
+        }
+        throw HttpError.from(error);
+      }
+    });
+  }
+
+  getBy(
+    index: keyof T,
+    value: T[keyof T] & Deno.KvKeyPart,
+  ): Promise<T> {
+    return startActiveSpan("getBy", async (span) => {
+      span.setAttribute("service", this.name);
+      span.setAttribute("index", String(index));
+      span.setAttribute("value", String(value));
+      if (
+        index !== "id" &&
+        !this.uniqueIndexes.includes(index as Exclude<keyof T, "id">)
+      ) {
         throw new HttpError(
           400,
-          `Invalid ${this.name} data: ${messages.join(", ")}`,
+          `Index "${
+            String(index)
+          }" is not a valid unique index for ${this.name}. Valid unique indexes are: ${
+            this.uniqueIndexes.join(", ")
+          }.`,
         );
       }
-      throw HttpError.from(error);
-    }
+      const kv = await this.getKv();
+      const entry = await kv.get<T>([this.name, index, value]);
+      if (!entry.value) {
+        throw new HttpError(
+          404,
+          `Failed to find ${this.name} by ${String(index)}`,
+        );
+      }
+      return entry.value;
+    });
   }
 
-  async getBy(
-    index: keyof T,
-    key: T[keyof T] & Deno.KvKeyPart,
-  ): Promise<T> {
-    if (
-      index !== "id" &&
-      !this.uniqueIndexes.includes(index as Exclude<keyof T, "id">)
-    ) {
-      throw new HttpError(
-        400,
-        `Index "${
-          String(index)
-        }" is not a valid unique index for ${this.name}. Valid unique indexes are: ${
-          this.uniqueIndexes.join(", ")
-        }.`,
-      );
-    }
-    const kv = await this.getKv();
-    const entry = await kv.get<T>([this.name, index, key]);
-    if (!entry.value) {
-      throw new HttpError(
-        404,
-        `Failed to find ${this.name} by ${String(index)}`,
-      );
-    }
-    return entry.value;
+  get(id: string): Promise<T> {
+    return startActiveSpan("get", async (span) => {
+      span.setAttribute("service", this.name);
+      span.setAttribute("id", id);
+      const kv = await this.getKv();
+      const entry = await kv.get<T>([this.name, "id", id]);
+      if (!entry.value) {
+        throw new HttpError(404, `Failed to find ${this.name}`);
+      }
+      return entry.value;
+    });
   }
 
-  async get(key: string): Promise<T> {
-    const kv = await this.getKv();
-    const entry = await kv.get<T>([this.name, "id", key]);
-    if (!entry.value) {
-      throw new HttpError(404, `Failed to find ${this.name}`);
-    }
-    return entry.value;
-  }
-
-  async list(
+  list(
     options?: ServiceListOptions<T>,
   ): Promise<{ entries: T[]; cursor?: string }> {
-    const { index, ...listOptions } = options || {};
-    const kv = await this.getKv();
+    return startActiveSpan("list", async (span) => {
+      span.setAttribute("service", this.name);
+      const { index, ...listOptions } = options || {};
+      span.setAttribute("index", String(index));
+      if (listOptions.cursor) {
+        span.setAttribute("cursor", listOptions.cursor);
+      }
+      if (listOptions.limit) {
+        span.setAttribute("limit", String(listOptions.limit));
+      }
+      if (listOptions.reverse) {
+        span.setAttribute("reverse", "true");
+      }
+      if (listOptions.batchSize) {
+        span.setAttribute("batchSize", String(listOptions.batchSize));
+      }
+      if (listOptions.consistency) {
+        span.setAttribute("consistency", listOptions.consistency);
+      }
+      const kv = await this.getKv();
 
-    if (
-      index &&
-      index !== "id" &&
-      !this.uniqueIndexes.includes(index as Exclude<keyof T, "id">) &&
-      !this.indexes.includes(index as Exclude<keyof T, "id">)
-    ) {
-      const validIndexes = ["id", ...this.uniqueIndexes, ...this.indexes];
-      throw new HttpError(
-        400,
-        `Index "${
-          String(index)
-        }" is not a valid index for ${this.name}. Valid indexes are: ${
-          validIndexes.join(", ")
-        }.`,
+      if (
+        index &&
+        index !== "id" &&
+        !this.uniqueIndexes.includes(index as Exclude<keyof T, "id">) &&
+        !this.indexes.includes(index as Exclude<keyof T, "id">)
+      ) {
+        const validIndexes = ["id", ...this.uniqueIndexes, ...this.indexes];
+        throw new HttpError(
+          400,
+          `Index "${
+            String(index)
+          }" is not a valid index for ${this.name}. Valid indexes are: ${
+            validIndexes.join(", ")
+          }.`,
+        );
+      }
+
+      const entries = kv.list<T>(
+        { prefix: [this.name, index ?? "id"] },
+        listOptions,
       );
-    }
-
-    const entries = kv.list<T>(
-      { prefix: [this.name, index ?? "id"] },
-      listOptions,
-    );
-    const values: T[] = [];
-    for await (const entry of entries) {
-      values.push(entry.value);
-    }
-    return { entries: values, cursor: entries.cursor };
+      const values: T[] = [];
+      for await (const entry of entries) {
+        values.push(entry.value);
+      }
+      return { entries: values, cursor: entries.cursor };
+    });
   }
 
-  async create(value: Omit<T, "id" | "createdAt" | "updatedAt">): Promise<T> {
-    const kv = await this.getKv();
-    const id: string = generateUUIDv7();
-    const now = new Date();
-    const created = this.parse({
-      ...value,
-      id,
-      createdAt: now,
-      updatedAt: now,
+  create(value: Omit<T, "id" | "createdAt" | "updatedAt">): Promise<T> {
+    return startActiveSpan("create", async (span) => {
+      span.setAttribute("service", this.name);
+      const id: string = generateUUIDv7();
+      span.setAttribute("id", id);
+      const kv = await this.getKv();
+      const now = new Date();
+      const created = this.parse({
+        ...value,
+        id,
+        createdAt: now,
+        updatedAt: now,
+      });
+      const transaction = kv.atomic();
+      for (const index of this.uniqueIndexes) {
+        const key = created[index] as Deno.KvKeyPart;
+        transaction.check({ key: [this.name, index, key], versionstamp: null });
+      }
+      transaction.set([this.name, "id", id], created);
+      for (const index of this.uniqueIndexes) {
+        const key = created[index] as Deno.KvKeyPart;
+        transaction.set([this.name, index, key], created);
+      }
+      for (const index of this.indexes) {
+        const key = created[index] as Deno.KvKeyPart;
+        transaction.set([this.name, index, key, id], created);
+      }
+      const result = await transaction.commit();
+      if (!result.ok) {
+        throw new HttpError(500, `Failed to create ${this.name}`);
+      }
+      return created;
     });
-    const transaction = kv.atomic();
-    for (const index of this.uniqueIndexes) {
-      const key = created[index] as Deno.KvKeyPart;
-      transaction.check({ key: [this.name, index, key], versionstamp: null });
-    }
-    transaction.set([this.name, "id", id], created);
-    for (const index of this.uniqueIndexes) {
-      const key = created[index] as Deno.KvKeyPart;
-      transaction.set([this.name, index, key], created);
-    }
-    for (const index of this.indexes) {
-      const key = created[index] as Deno.KvKeyPart;
-      transaction.set([this.name, index, key, id], created);
-    }
-    const result = await transaction.commit();
-    if (!result.ok) {
-      throw new HttpError(500, `Failed to create ${this.name}`);
-    }
-    return created;
   }
 
-  async update(value: Omit<T, "createdAt" | "updatedAt">): Promise<T> {
-    const kv = await this.getKv();
+  update(value: Omit<T, "createdAt" | "updatedAt">): Promise<T> {
+    return startActiveSpan("update", async (span) => {
+      span.setAttribute("service", this.name);
+      span.setAttribute("id", value.id);
+      const kv = await this.getKv();
 
-    const existing = await kv.get<T>([this.name, "id", value.id]);
-    if (!existing.value) {
-      throw new HttpError(404, `Failed to find ${this.name} to update`);
-    }
-
-    const updatedValue = this.parse({
-      ...value,
-      createdAt: existing.value.createdAt,
-      updatedAt: new Date(),
-    });
-
-    const transaction = kv.atomic();
-    transaction.check(existing);
-    for (const index of this.uniqueIndexes) {
-      const currentKey = existing.value?.[index] as Deno.KvKeyPart;
-      const nextKey = updatedValue[index] as Deno.KvKeyPart;
-      if (nextKey !== currentKey) {
-        transaction.check({
-          key: [this.name, index, nextKey],
-          versionstamp: null,
-        });
+      const existing = await kv.get<T>([this.name, "id", value.id]);
+      if (!existing.value) {
+        throw new HttpError(404, `Failed to find ${this.name} to update`);
       }
-    }
 
-    transaction.set(
-      [this.name, "id", updatedValue.id],
-      updatedValue,
-    );
-    for (const index of this.uniqueIndexes) {
-      const currentKey = existing.value?.[index] as Deno.KvKeyPart;
-      const nextKey = updatedValue[index] as Deno.KvKeyPart;
-      if (nextKey !== currentKey) {
-        transaction.delete([this.name, index, currentKey]);
+      const updatedValue = this.parse({
+        ...value,
+        createdAt: existing.value.createdAt,
+        updatedAt: new Date(),
+      });
+
+      const transaction = kv.atomic();
+      transaction.check(existing);
+      for (const index of this.uniqueIndexes) {
+        const currentKey = existing.value?.[index] as Deno.KvKeyPart;
+        const nextKey = updatedValue[index] as Deno.KvKeyPart;
+        if (nextKey !== currentKey) {
+          transaction.check({
+            key: [this.name, index, nextKey],
+            versionstamp: null,
+          });
+        }
       }
-      transaction.set([this.name, index, nextKey], updatedValue);
-    }
-    for (const index of this.indexes) {
-      const currentKey = existing.value?.[index] as Deno.KvKeyPart;
-      const nextKey = updatedValue[index] as Deno.KvKeyPart;
-      if (existing.value && currentKey !== nextKey) {
-        transaction.delete([
-          this.name,
-          index,
-          currentKey,
-          existing.value.id,
-        ]);
-      }
+
       transaction.set(
-        [this.name, index, nextKey, updatedValue.id],
+        [this.name, "id", updatedValue.id],
         updatedValue,
       );
-    }
+      for (const index of this.uniqueIndexes) {
+        const currentKey = existing.value?.[index] as Deno.KvKeyPart;
+        const nextKey = updatedValue[index] as Deno.KvKeyPart;
+        if (nextKey !== currentKey) {
+          transaction.delete([this.name, index, currentKey]);
+        }
+        transaction.set([this.name, index, nextKey], updatedValue);
+      }
+      for (const index of this.indexes) {
+        const currentKey = existing.value?.[index] as Deno.KvKeyPart;
+        const nextKey = updatedValue[index] as Deno.KvKeyPart;
+        if (existing.value && currentKey !== nextKey) {
+          transaction.delete([
+            this.name,
+            index,
+            currentKey,
+            existing.value.id,
+          ]);
+        }
+        transaction.set(
+          [this.name, index, nextKey, updatedValue.id],
+          updatedValue,
+        );
+      }
 
-    const result = await transaction.commit();
-    if (!result.ok) {
-      throw new HttpError(500, `Failed to update ${this.name}`);
-    }
+      const result = await transaction.commit();
+      if (!result.ok) {
+        throw new HttpError(500, `Failed to update ${this.name}`);
+      }
 
-    return updatedValue;
+      return updatedValue;
+    });
   }
 
-  async patch(
+  patch(
     partialValue: Omit<Partial<T>, "createdAt" | "updatedAt"> & { id: string },
   ): Promise<T> {
-    const kv = await this.getKv();
+    return startActiveSpan("patch", async (span) => {
+      span.setAttribute("service", this.name);
+      span.setAttribute("id", partialValue.id);
+      const kv = await this.getKv();
 
-    const existing = await kv.get<T>([this.name, "id", partialValue.id]);
-    if (!existing.value) {
-      throw new HttpError(404, `Failed to find ${this.name} to patch`);
-    }
+      const existing = await kv.get<T>([this.name, "id", partialValue.id]);
+      if (!existing.value) {
+        throw new HttpError(404, `Failed to find ${this.name} to patch`);
+      }
 
-    const newValue = this.parse({
-      ...existing.value,
-      ...partialValue,
-      updatedAt: new Date(),
+      const newValue = this.parse({
+        ...existing.value,
+        ...partialValue,
+        updatedAt: new Date(),
+      });
+
+      const transaction = kv.atomic();
+      transaction.check(existing);
+      for (const index of this.uniqueIndexes) {
+        const currentKey = existing.value?.[index] as Deno.KvKeyPart;
+        const nextKey = newValue[index] as Deno.KvKeyPart;
+        if (nextKey !== currentKey) {
+          transaction.check({
+            key: [this.name, index, nextKey],
+            versionstamp: null,
+          });
+        }
+      }
+
+      transaction.set([this.name, "id", newValue.id], newValue);
+      for (const index of this.uniqueIndexes) {
+        const currentKey = existing.value?.[index] as Deno.KvKeyPart;
+        const nextKey = newValue[index] as Deno.KvKeyPart;
+        if (nextKey !== currentKey) {
+          transaction.delete([this.name, index, currentKey]);
+        }
+        transaction.set([this.name, index, nextKey], newValue);
+      }
+      for (const index of this.indexes) {
+        const currentKey = existing.value?.[index] as Deno.KvKeyPart;
+        const nextKey = newValue[index] as Deno.KvKeyPart;
+        if (existing.value && currentKey !== nextKey) {
+          transaction.delete([
+            this.name,
+            index,
+            currentKey,
+            existing.value.id,
+          ]);
+        }
+        transaction.set([this.name, index, nextKey, newValue.id], newValue);
+      }
+
+      const result = await transaction.commit();
+      if (!result.ok) {
+        throw new HttpError(500, `Failed to patch ${this.name}`);
+      }
+
+      return newValue;
     });
-
-    const transaction = kv.atomic();
-    transaction.check(existing);
-    for (const index of this.uniqueIndexes) {
-      const currentKey = existing.value?.[index] as Deno.KvKeyPart;
-      const nextKey = newValue[index] as Deno.KvKeyPart;
-      if (nextKey !== currentKey) {
-        transaction.check({
-          key: [this.name, index, nextKey],
-          versionstamp: null,
-        });
-      }
-    }
-
-    transaction.set([this.name, "id", newValue.id], newValue);
-    for (const index of this.uniqueIndexes) {
-      const currentKey = existing.value?.[index] as Deno.KvKeyPart;
-      const nextKey = newValue[index] as Deno.KvKeyPart;
-      if (nextKey !== currentKey) {
-        transaction.delete([this.name, index, currentKey]);
-      }
-      transaction.set([this.name, index, nextKey], newValue);
-    }
-    for (const index of this.indexes) {
-      const currentKey = existing.value?.[index] as Deno.KvKeyPart;
-      const nextKey = newValue[index] as Deno.KvKeyPart;
-      if (existing.value && currentKey !== nextKey) {
-        transaction.delete([
-          this.name,
-          index,
-          currentKey,
-          existing.value.id,
-        ]);
-      }
-      transaction.set([this.name, index, nextKey, newValue.id], newValue);
-    }
-
-    const result = await transaction.commit();
-    if (!result.ok) {
-      throw new HttpError(500, `Failed to patch ${this.name}`);
-    }
-
-    return newValue;
   }
 
-  async delete(id: string): Promise<void> {
-    const kv = await this.getKv();
+  delete(id: string): Promise<void> {
+    return startActiveSpan("delete", async (span) => {
+      span.setAttribute("service", this.name);
+      span.setAttribute("id", id);
+      const kv = await this.getKv();
 
-    const existingEntry = await kv.get<T>([this.name, "id", id]);
-    if (!existingEntry.value) {
-      throw new HttpError(404, `Failed to find ${this.name} to delete`);
-    }
+      const existingEntry = await kv.get<T>([this.name, "id", id]);
+      if (!existingEntry.value) {
+        throw new HttpError(404, `Failed to find ${this.name} to delete`);
+      }
 
-    const existingValue = existingEntry.value;
-    const transaction = kv.atomic();
-    transaction.check(existingEntry);
+      const existingValue = existingEntry.value;
+      const transaction = kv.atomic();
+      transaction.check(existingEntry);
 
-    transaction.delete([this.name, "id", id]);
-    for (const index of this.uniqueIndexes) {
-      const key = existingValue[index] as Deno.KvKeyPart;
-      transaction.delete([this.name, index, key]);
-    }
-    for (const index of this.indexes) {
-      const key = existingValue[index] as Deno.KvKeyPart;
-      transaction.delete([this.name, index, key, id]);
-    }
+      transaction.delete([this.name, "id", id]);
+      for (const index of this.uniqueIndexes) {
+        const key = existingValue[index] as Deno.KvKeyPart;
+        transaction.delete([this.name, index, key]);
+      }
+      for (const index of this.indexes) {
+        const key = existingValue[index] as Deno.KvKeyPart;
+        transaction.delete([this.name, index, key, id]);
+      }
 
-    const result = await transaction.commit();
-    if (!result.ok) {
-      throw new HttpError(500, `Failed to delete ${this.name}`);
-    }
+      const result = await transaction.commit();
+      if (!result.ok) {
+        throw new HttpError(500, `Failed to delete ${this.name}`);
+      }
+    });
   }
 }
