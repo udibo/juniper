76e2f322b15f5259d11b5b8c1daa3505ce563ce0
Kyle June <github@kylejune.com>
2025-05-24 20:06:01 -0400

    Remove debug logging for serveStatic issue

--- server.ts
+++ server.ts
@@ -73,28 +73,12 @@ function buildAppFromRoutes<
       ? relativePath.replace(/\\/g, "/")
       : relativePath;
 
-    // Temporary debugging for Windows CI
-    console.log("Static path debug:", {
-      mainUrl,
-      dirname,
-      absolutePublicPath,
-      cwd,
-      relativePath,
-      publicPath,
-      os: Deno.build.os,
-    });
-
     // Note: There's a known issue with Hono's serveStatic on Windows (https://github.com/honojs/hono/issues/3475)
     // This workaround attempts to work around path separator and relative path issues
     app.get(
       "*",
       serveStatic({
         root: publicPath,
-        onNotFound: (path, c) => {
-          console.error(
-            `Static file not found: ${path}, request path: ${c.req.path}, publicPath: ${publicPath}, cwd: ${cwd}`,
-          );
-        },
       }),
     );
   }
